24f937c15d22911115d7ef2c9246b3e56f20c72b
Maximilien <maximilien.raes@itctc.be>
2020-02-27 11:54:05 +0100

    fix code

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import { UserAgentApplication } from 'msal'
 import { msalConfig } from './config'
 // Ce module permet d'encapsuler la librairie msal.js
 // Grâce à ce module, la connexion et récupération de token est simplifiée
-// Les token récupéré (msal-access-token) permettent d'effectuer des requêtes vers Graph, SharePoint, ...
+// Les token récupéré (msal-access-token) permettent d'effectuer des requêtes
+// Sur l'API GRAPH
 
 
 /** Méthode de login en utilisant les fonctions msal
@@ -14,46 +15,53 @@ import { msalConfig } from './config'
  *  et des scopes authorisés
  */
 export const msalLogin = (clientId, tenantId, scopes) => {
-    return new Promise(async (resolve, reject) => {
+    return new Promise((resolve, reject) => {
         const msalLoginAgent = new UserAgentApplication(msalConfig(clientId, tenantId)); // Déclaration de l'agent de login
-        msalLoginAgent.handleAuthenticationResponse(async (error, response) => {
-            try {
-                const token = await getAccessToken(msalLoginAgent, scopes); // Token
-                await tokenToCache(token); // Cache token
-                return resolve({ accessToken: `Bearer ${token}` }); // Resolve token
-            } catch (err) {
-                reject(err);
-            }
-        });
+        msalLoginAgent.handleRedirectCallback((error, response) => {
+            return getAccessToken(msalLoginAgent, scopes) // Récupération du jeton d'accès
+                .then(response => {
+                    tokenToCache(response).then(() => {
+                        return resolve({ accessToken: `Bearer ${response}` }); // on renvoit la réponse
+                    })
+                })
+                .catch(err => {
+                    return reject(err);
+                });
+        }); // Déclaration de la gestion de callback
         if (msalLoginAgent.getAccount()) {
-            try {
-                const token = await getAccessToken(msalLoginAgent, scopes); // Token
-                await tokenToCache(token); // Cache token
-                return resolve({ accessToken: `Bearer ${token}` }); // Resolve token
-            } catch (err) {
-                reject(err);
-            }
+            // Le compte est disponible
+            return getAccessToken(msalLoginAgent, scopes) // Récupération du jeton d'accès
+                .then(response => {
+                    tokenToCache(response).then(() => {
+                        return resolve({ accessToken: `Bearer ${response}` }); // on renvoit la réponse
+                    })
+                })
+                .catch(err => {
+                    return reject(err);
+                });
         } else {
-            return msalLoginAgent.loginRedirect(scopes); // Login requis
+            // Il faut un login
+            return msalLoginAgent.loginRedirect(scopes);
         }
-    })
-}
+    });
+};
 
 /** Récupération du token d'accès à utiliser pour les appels rest, graph, ... 
  * Cette fonction est une promesse
 */
 const getAccessToken = (msalLoginAgent, scopes) => {
-    return new Promise(async (resolve, reject) => {
+    return new Promise((resolve, reject) => {
         try {
             const token = acquireTokenSilent(); // Custom call to acquiretoken silent function
-            resolve(token); // Token found, resolve it
+            resolve(token); // Token foudn, resolve it
         } catch (err) {
-            try {
-                const { accessToken } = await msalLoginAgent.loginRedirect(scopes); // Redirect to login
-                resolve(accessToken); // Resolve token found
-            } catch (err) {
-                return reject(err);
-            }
+            return msalLoginAgent.loginRedirect(scopes)
+                .then(response => {
+                    return resolve(response.accessToken) // response contient accessToken
+                })
+                .catch(err => {
+                    return reject(err)
+                });
         }
     });
 };
@@ -85,13 +93,13 @@ const acquireTokenSilent = () => {
     }
     if (token) return token; // If token exist, return it
     throw new Error('No valid token found'); // Else trhow error, there was no token
-}
+};
 
 /** Mise en cache des jetons
  * Fonction retournant une promesse (async)
  */
 const tokenToCache = (token) => {
-    return asyncLocalStorage.setItem('msal-access-token', `Bearer ${token}`) // Mise en local storage
+    return asyncLocalStorage.setItem('msal-access-token', `Bearer ${token}`); // Mise en local storage
 }
 
 /** Fonction asyncrone du local storage */
